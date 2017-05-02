c5b4284449d19e64025a3833ee29f82c08a8cea3
dbl4k <github@dbl4k.net>
2017-05-03 00:20:39 +0100

    initial commit

---
+++ ViewModels/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogApp.ViewModels
+{
+    public class Article
+    {
+        public string Title;
+        public string Content;
+
+    }
+}
